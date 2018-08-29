84dcab0d0f11ae83f17f19876da75425558988f0
alexneustein <alex.neustein@gmail.com>
2018-08-29 16:38:30 -0400

    Crushed it

--- src/components/App.js
+++ src/components/App.js
@@ -15,6 +15,40 @@ class App extends React.Component {
     }
   }
 
+  onChangeType = (type) => {
+    this.setState({
+      filters: {
+        ...this.state.filters,
+        type: type,
+      },
+    });
+  }
+
+  onFindPetsClick = () => {
+
+    let query
+    if ((this.state.filters.type === 'all') || (this.state.filters.type === undefined)) {
+      query = "/api/pets"
+    } else {
+      query = `/api/pets?type=${this.state.filters.type}`
+    }
+    return fetch(query, {
+
+    }, {method: "GET"})
+    .then(r => r.json()).then(r => {
+      this.setState({pets: r})
+    })
+  }
+
+  onAdoptPet = (e) => {
+    let adoptedPet = this.state.pets.find((p) => {
+      return (p.id === e)
+    })
+    adoptedPet.isAdopted = true
+    this.setState(adoptedPet)
+  }
+
+// this.setState({pets: r})
   render() {
     return (
       <div className="ui container">
@@ -24,10 +58,10 @@ class App extends React.Component {
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+              <Filters onChangeType={this.onChangeType} onFindPetsClick={this.onFindPetsClick}/>
             </div>
             <div className="twelve wide column">
-              <PetBrowser />
+              <PetBrowser pets={this.state.pets} onAdoptPet={this.onAdoptPet}/>
             </div>
           </div>
         </div>
--- src/components/PetBrowser.js
+++ src/components/PetBrowser.js
@@ -3,8 +3,17 @@ import React from 'react'
 import Pet from './Pet'
 
 class PetBrowser extends React.Component {
+
   render() {
-    return <div className="ui cards">PET COMPONENT SHOULD GO HERE</div>
+    return (<div className="ui cards">
+
+    {this.props.pets.map(p => {
+      // console.log(p)
+      return (<div><Pet pet={p} onAdoptPet={this.props.onAdoptPet} isAdopted={p.isAdopted}/> </div>)
+    })}
+
+      </div>
+    )
   }
 }
 
